e1fe8df85b2da226a1a02a655a5380475b3ed118
kusa1 <kusakabe+1@gworks.jp>
2017-06-07 22:21:45 +0900

    Fixed Shibata's comment
    -fixed behavior when error occured

--- 16445128.sh
+++ 16445128.sh
@@ -11,21 +11,21 @@ val2=$2
 
 expr ${val1} + 1 > /dev/null 2>&1
 if [ $? -eq 2 ] ; then
-  echo "最初の引数が数字ではありません"
+  echo "最初の引数が数字ではありません" 1>&2
   exit 1
 fi
 expr ${val2} + 1 > /dev/null 2>&1
 if [ $? -eq 2 ] ; then
-  echo "二つ目の引数が数字ではありません"
+  echo "二つ目の引数が数字ではありません" 1>&2
   exit 1
 fi
 
 if [ 0 -eq "$val1" ]; then
-  echo "最初の引数が自然数ではありません"
+  echo "最初の引数が自然数ではありません" 1>&2
   exit 1
 fi
 if [ "$val2" -eq 0 ]; then
-  echo "二つ目の引数が自然数ではありません"
+  echo "二つ目の引数が自然数ではありません" 1>&2
   exit 1
 fi
 
